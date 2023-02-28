e208b57be843fd36bfe47a90b96c2403666b567e
patelaftab <10patelaftab@gmail.com>
2023-02-28 08:56:08 +0530

    [Aftab] : Cheking Regex Condition FOr VAlid First Name

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regexproblem regexproblem = new Regexproblem();
+            regexproblem.Validatefirstname("Aft");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexProblem/Regex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class Regexproblem
+    {
+        public void Validatefirstname(string name)
+        {
+            string firstName = "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, firstName)) 
+            {
+                Console.WriteLine("The Name is MAtching");
+            }
+            else
+            {
+                Console.WriteLine("The Regex Pattern is not WOrking");
+            }
+            
+           
+        }
+        public void Validatelastname(string name)
+        {
+            string lastName= "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, lastName)) 
+            {
+                Console.WriteLine("The Name is MAtching");
+            }
+            else
+            {
+                Console.WriteLine("The PAttern is Not MAtching");
+            }
+            
+        }
+    }
+}
